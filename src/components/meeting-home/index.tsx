f8366ede9d5ffe3302f16dc8a3ca3ba8f1c4e89c
aprile-0401 <xin.wang@target-energysolutions.com>
2020-06-30 18:17:27 +0800

    chore: add eslint WIP

--- src/components/meeting-home/index.tsx
+++ src/components/meeting-home/index.tsx
@@ -3,33 +3,26 @@ import './styles.scss';
 
 // const { useState, useCallback, useRef } = React
 
-export const MeetingHome: React.FunctionComponent = () => {
-  
+const MeetingHome: React.FunctionComponent = () => (
+  <div className="app--meeting-home-top">
+    <ul className="app--meeting-home-top-operate">
+      <li
+        className="app--meeting-home-top-operate-quick-meeting"
+      >
+        <p className="app--meeting-home-top-operate-text">Quick Meiii</p>
+      </li>
+      <li
+        className="app--meeting-home-top-operate-join-meeting"
+      >
+        <p className="app--meeting-home-top-operate-text">Join a Meeting</p>
+      </li>
+      <li
+        className="app--meeting-home-top-operate-schedule-meeting"
+      >
+        <p className="app--meeting-home-top-operate-text">Schedule a Meeting</p>
+      </li>
+    </ul>
 
-  return (
-      <div className="app--meeting-home-top">
-        <ul className="app--meeting-home-top-operate">
-          <li
-            className="app--meeting-home-top-operate-quick-meeting"
-            // onClick={handleCreateQuickMeeting}
-          >
-            <p className="app--meeting-home-top-operate-text">Quick Meiii</p>
-          </li>
-          <li
-            className="app--meeting-home-top-operate-join-meeting"
-            // onClick={toggleJoinDialogVisible}
-          >
-            <p className="app--meeting-home-top-operate-text">Join a Meeting</p>
-          </li>
-          <li
-            className="app--meeting-home-top-operate-schedule-meeting"
-            // onClick={toggleScheduleDialogVisible}
-          >
-            <p className="app--meeting-home-top-operate-text">Schedule a Meeting</p>
-          </li>
-        </ul>
-        
-      </div>
-    )
-};
-
+  </div>
+);
+export default MeetingHome;

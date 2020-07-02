fdc8dda1ece4e7b542cb74ca20787a3de8f4563c
Wang Xin <583623148@qq.com>
2020-07-02 22:51:01 +0800

    feat: replace uglify with terser

--- src/components/meeting-list/index.tsx
+++ src/components/meeting-list/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import './styles.scss'
 import { useToast } from '@target-energysolutions/toast'
+import { map } from 'lodash-es'
 
 // const { useState, useCallback, useRef } = React
-
+const themes = ['red', 'yellow', 'black']
 const MeetingList: React.FunctionComponent = () => {
   
 console.log('this was refeshed')
@@ -12,6 +13,9 @@ const showToast = useToast()
       <div className="app--meeting-list-container">
         I am tes
         <button onClick={() => showToast({ type: 'confirm', text: 'hello world'})}>add a toast</button>
+        {
+          map(themes, (item: string) => <span>{item}</span>)
+        }
       </div>
     )
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,19 +1,30 @@
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.base.js');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin({
-        sourceMap: true
-    }),
+    // new UglifyJSPlugin({
+    //     sourceMap: true,
+    //     parallel: true,
+    // }),
          new webpack.DefinePlugin({
               'process.env.NODE_ENV': JSON.stringify('production')
         })
   ],
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [
+  //     new TerserPlugin({
+  //       cache: true,
+  //       parallel: true,
+  //     })
+  //   ]
+  // }
   // optimization: {
   //   splitChunks: {
   //     cacheGroups: {
